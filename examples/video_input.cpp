d88842a8c2d824586a88abaca07f3516184933d2
jiapei100 <jp4work@gmail.com>
2016-08-11 00:42:50 -0700

    Remove all 'using namespace'

--- examples/video_input.cpp
+++ examples/video_input.cpp
@@ -21,15 +21,13 @@
 #include "Flandmark.h"
 #include "helpers.h"
 
-using namespace std;
-using namespace cv;
-using namespace clandmark;
+
 
 /** Global variables */
-String face_cascade_name = "haarcascade_frontalface_alt.xml";
-CascadeClassifier face_cascade;
-string window_name = "flandmark - video input";
-RNG rng(12345);
+cv::String face_cascade_name = "haarcascade_frontalface_alt.xml";
+cv::CascadeClassifier face_cascade;
+std::string window_name = "flandmark - video input";
+cv::RNG rng(12345);
 
 
 cimg_library::CImg<unsigned char> * cvImgToCImg(cv::Mat &cvImg)
@@ -45,7 +43,7 @@ cimg_library::CImg<unsigned char> * cvImgToCImg(cv::Mat &cvImg)
 
 cv::Mat & CImgtoCvImg(cv::Mat &result, cimg_library::CImg<unsigned char> *img)
 {
-	result = cv::Mat(img->height(), img->width(), CV_8U);
+    result = cv::Mat(img->height(), img->width(), CV_8U);
 
 	for (int x=0; x < result.cols; ++x)
 		for (int y=0; y < result.rows; ++y)
@@ -55,20 +53,20 @@ cv::Mat & CImgtoCvImg(cv::Mat &result, cimg_library::CImg<unsigned char> *img)
 }
 
 /** @function detectAndDisplay */
-void detectAndDisplay( Mat &frame, Flandmark *flandmark)
+void detectAndDisplay( cv::Mat &frame, clandmark::Flandmark *flandmark)
 {
-	std::vector<Rect> faces;
-	Mat frame_gray;
+    std::vector<cv::Rect> faces;
+    cv::Mat frame_gray;
 //	int bbox[4];
 	int bbox[8];
-	fl_double_t *landmarks;
+    clandmark::fl_double_t *landmarks;
 
-	cvtColor( frame, frame_gray, CV_BGR2GRAY );
+    cv::cvtColor( frame, frame_gray, CV_BGR2GRAY );
 //    cvtColor( frame, frame_gray, COLOR_BGR2GRAY );
-	equalizeHist( frame_gray, frame_gray );
+    cv::equalizeHist( frame_gray, frame_gray );
 
 	//-- Detect faces
-	face_cascade.detectMultiScale( frame_gray, faces, 1.1, 2, 0|CV_HAAR_SCALE_IMAGE, Size(30, 30) );
+    face_cascade.detectMultiScale( frame_gray, faces, 1.1, 2, 0|CV_HAAR_SCALE_IMAGE, cv::Size(30, 30) );
 //	face_cascade.detectMultiScale( frame_gray, faces, 1.1, 2, 0|CASCADE_SCALE_IMAGE, Size(30, 30) );
 
 //    CImage *frm_gray = new CImage();
@@ -96,17 +94,17 @@ void detectAndDisplay( Mat &frame, Flandmark *flandmark)
 		landmarks = flandmark->getLandmarks();
 
 		// Draw bounding box and detected landmarks
-//		rectangle(frame, Point(bbox[0], bbox[1]), Point(bbox[2], bbox[3]), Scalar(255, 0, 0));
-		circle(frame, Point(int(landmarks[0]), int(landmarks[1])), 2, Scalar(255, 0, 0), -1);
+//		cv::Rectangle(frame, Point(bbox[0], bbox[1]), Point(bbox[2], bbox[3]), Scalar(255, 0, 0));
+        cv::circle(frame, cv::Point(int(landmarks[0]), int(landmarks[1])), 2, cv::Scalar(255, 0, 0), -1);
 		for (int i=2; i < 2*flandmark->getLandmarksCount(); i+=2)
 		{
-			circle(frame, Point(int(landmarks[i]), int(landmarks[i+1])), 2, Scalar(0, 0, 255), -1);
+            cv::circle(frame, cv::Point(int(landmarks[i]), int(landmarks[i+1])), 2, cv::Scalar(0, 0, 255), -1);
 		}
 
 		// Textual output
-		printTimingStats(flandmark->timings);
-		printLandmarks(landmarks, flandmark->getLandmarksCount());
-		printLandmarks(flandmark->getLandmarksNF(), flandmark->getLandmarksCount());
+        clandmark::printTimingStats(flandmark->timings);
+        clandmark::printLandmarks(landmarks, flandmark->getLandmarksCount());
+        clandmark::printLandmarks(flandmark->getLandmarksNF(), flandmark->getLandmarksCount());
 	}
 
 //    delete frm_gray;
@@ -117,36 +115,36 @@ int main( int argc, const char** argv )
 {
 	if (argc < 5)
 	{
-		cerr << "Usage: video_input <path_to_haarcasade> <flandmark_model.xml> { cam | vid } [ filename | cam_id ] [ output_filename ]" << endl;
+        std::cerr << "Usage: video_input <path_to_haarcasade> <flandmark_model.xml> { cam | vid } [ filename | cam_id ] [ output_filename ]" << std::endl;
 		return -1;
 	}
 
 	//CvCapture* capture = 0x0;
 	//CvVideoWriter* writer = 0x0;
-	VideoCapture capture;
-	VideoWriter writer;
+    cv::VideoCapture capture;
+    cv::VideoWriter writer;
 
-	Mat frame;
+    cv::Mat frame;
 
 	double tic;
 	int camID = -1;
-	string str_type = argv[3];
+    std::string str_type = argv[3];
 	int type = -1;
 	bool saveoutput = false;
-	string out_fname;
+    std::string out_fname;
 
-	Flandmark *flandmark = Flandmark::getInstanceOf(argv[2]);
+    clandmark::Flandmark *flandmark = clandmark::Flandmark::getInstanceOf(argv[2]);
 
 	if (!flandmark)
 	{
-		cerr << "Usage: video_input <path_to_haarcascade> <flandmark_model.xml> { cam | vid } [ filename | cam_id ] [ output_filename ]" << endl;
+        std::cerr << "Usage: video_input <path_to_haarcascade> <flandmark_model.xml> { cam | vid } [ filename | cam_id ] [ output_filename ]" << std::endl;
 		return -1;
 	}
 
 	//-- 1. Load the cascades
 	if( !face_cascade.load( argv[1]+face_cascade_name ) )
 	{
-		cerr << "Couldn't load the haar cascade. Exiting..." << endl;
+        std::cerr << "Couldn't load the haar cascade. Exiting..." << std::endl;
 		return -1;
 	};
 
@@ -171,14 +169,14 @@ int main( int argc, const char** argv )
 				capture.open(argv[4]);
 			break;
 			default:
-				cerr << "Usage: video_input <path_to_haarcascade> <flandmark_model.xml> { cam | vid } [ filename | cam_id ] [ output_filename ]" << endl;
+                std::cerr << "Usage: video_input <path_to_haarcascade> <flandmark_model.xml> { cam | vid } [ filename | cam_id ] [ output_filename ]" << std::endl;
 				return -1;
 			break;
 		}
 
 		if (!capture.isOpened())
 		{
-			cerr << "Could not open the video input. Exiting..." << endl;
+            std::cerr << "Could not open the video input. Exiting..." << std::endl;
 			return -1;
 		}
 
@@ -186,7 +184,7 @@ int main( int argc, const char** argv )
 
 		if (frame.empty())
 		{
-			cerr << "Unable to read frame. Exiting..." << endl;
+            std::cerr << "Unable to read frame. Exiting..." << std::endl;
 			return -1;
 		}
 
@@ -203,14 +201,14 @@ int main( int argc, const char** argv )
 						//CV_FOURCC('M', 'J', 'P', 'G'),
 						capture.get(CV_CAP_PROP_FPS),
 //                        capture.get(CAP_PROP_FPS),
-						Size(capture.get(CV_CAP_PROP_FRAME_WIDTH), capture.get(CV_CAP_PROP_FRAME_HEIGHT))
+                        cv::Size(capture.get(CV_CAP_PROP_FRAME_WIDTH), capture.get(CV_CAP_PROP_FRAME_HEIGHT))
 //                        Size(capture.get(CAP_PROP_FRAME_WIDTH), capture.get(CAP_PROP_FRAME_HEIGHT))
 					);
 		}
 	}
 
 	// window
-	namedWindow(window_name, CV_WINDOW_KEEPRATIO);
+    cv::namedWindow(window_name, CV_WINDOW_KEEPRATIO);
 //    namedWindow(window_name, WINDOW_KEEPRATIO);
 
 	//-- 2. Read the video stream
@@ -218,7 +216,7 @@ int main( int argc, const char** argv )
 	{
 		while( true )
 		{
-			tic = (double)getTickCount();
+            tic = (double)cv::getTickCount();
 
 			capture >> frame;
 
@@ -227,17 +225,17 @@ int main( int argc, const char** argv )
 			{
 				detectAndDisplay( frame, flandmark );
 
-				tic = ((double)getTickCount() - tic)/getTickFrequency() * 1000;
+                tic = ((double)cv::getTickCount() - tic)/cv::getTickFrequency() * 1000;
 
-				stringstream fps;
-				fps << "fps: " << setprecision(4) << setw(4) << 1000.0 / tic << " ";
-				putText(frame, fps.str(), Point(10, 25), CV_FONT_HERSHEY_COMPLEX, 1, Scalar(255, 255, 0));
+                std::stringstream fps;
+                fps << "fps: " << std::setprecision(4) << std::setw(4) << 1000.0 / tic << " ";
+                cv::putText(frame, fps.str(), cv::Point(10, 25), CV_FONT_HERSHEY_COMPLEX, 1, cv::Scalar(255, 255, 0));
 //				putText(frame, fps.str(), Point(10, 25), FONT_HERSHEY_COMPLEX, 1, Scalar(255, 255, 0));
 
 				imshow( window_name, frame );
 
 			} else {
-				cerr << "No frame --- break." << endl;
+                std::cerr << "No frame --- break." << std::endl;
 				break;
 			}
 
@@ -246,7 +244,7 @@ int main( int argc, const char** argv )
 				writer << frame;
 			}
 
-			int c = waitKey(10);
+            int c = cv::waitKey(10);
 			if( (char)c == 'c' )
 			{
 				break;
